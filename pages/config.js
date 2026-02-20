42c561f5f442cc666f1501d3f40103cff24a0b20
BBISMblockay <blake@baseballism.com>
2026-02-19 19:59:04 -0800

    Create config.js

---
+++ pages/config.js
@@ -0,0 +1,7 @@
+// /public/config.js
+window.__SILO_CONFIG__ = {
+  SUPABASE_URL: "https://mkquclffrvlzyecnabyf.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1rcXVjbGZmcnZsenllY25hYnlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEzODk4MDEsImV4cCI6MjA4Njk2NTgwMX0.vkOceXSXLnUMPp5FwvivcFvFcDxuVyQnlmmRc9hp1V4",
+  REDIRECT_TO: "/executive.html",
+  EXPECT_EMAIL_CONFIRMATION: true
+};
