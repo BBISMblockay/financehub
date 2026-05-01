d9df98b94e54d9451771585b9511132b73406ae0
BBISMblockay <blake@baseballism.com>
2026-05-01 14:43:30 -0700

    Change REDIRECT_TO path from executive to finance

--- pages/config.js
+++ pages/config.js
@@ -2,6 +2,6 @@
 window.__SILO_CONFIG__ = {
   SUPABASE_URL: "https://mkquclffrvlzyecnabyf.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1rcXVjbGZmcnZsenllY25hYnlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEzODk4MDEsImV4cCI6MjA4Njk2NTgwMX0.vkOceXSXLnUMPp5FwvivcFvFcDxuVyQnlmmRc9hp1V4",
-  REDIRECT_TO: "/executive.html",
+  REDIRECT_TO: "/finance.html",
   EXPECT_EMAIL_CONFIRMATION: true
 };
